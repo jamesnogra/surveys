dd2f2caa61499837215c2f1ccc80f9392a7464cd
James Arnold <love.gerald@yahoo.com>
2016-03-26 09:50:50 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'survey_db'),
             'username'  => env('DB_USERNAME', 'surveys'),
-            'password'  => env('DB_PASSWORD', 'surveys123'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
